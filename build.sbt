e239250da30f9d60761c202c0a09694888ad6baf
Rob Bart <rbart@cs.washington.edu>
2013-06-17 14:28:48 -0700

    Created KbpSentence, added ScalaTest to build.sbt.

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ scalaVersion := "2.10.2"
 organization := "edu.knowitall"
 
 libraryDependencies += "edu.stanford.nlp" % "stanford-corenlp" % "1.3.4"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
---
+++ src/main/scala/edu/knowitall/tac2013/openie/KbpSentence.scala
@@ -0,0 +1,25 @@
+package edu.knowitall.tac2013.openie
+
+case class KbpSentence(val docId: String, val sentId: Int, val text: String) {
+
+}
+
+object KbpSentence {
+  
+  private val tabRegex = "\t".r
+  
+  def read(pickle: String): Option[KbpSentence] = {
+    tabRegex.split(pickle) match {
+      case Array(docId, sentIdString, text, _*) => Some(KbpSentence(docId, sentIdString.toInt, text))
+      case _ => {
+        System.err.println("Error reading KbpSentence: %s".format(pickle))
+        None
+      }
+    }
+  }
+  
+  def write(sent: KbpSentence): String = {
+    val fields = KbpSentence.unapply(sent).get.productIterator.map(_.toString).toSeq
+    fields.map(_.replaceAll("\t", "")).mkString("\t")
+  }
+}
