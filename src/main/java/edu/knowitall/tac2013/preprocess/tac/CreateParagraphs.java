78e7c6892857bffdc80739f57789568bb1747580
Rob Bart <rbart@cs.washington.edu>
2013-06-17 12:19:03 -0700

    Improved CLI for CreateParagraphs.

--- src/main/java/edu/knowitall/tac2013/preprocess/tac/CreateParagraphs.java
+++ src/main/java/edu/knowitall/tac2013/preprocess/tac/CreateParagraphs.java
@@ -3,40 +3,68 @@ package edu.knowitall.tac2013.preprocess.tac;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 
 public class CreateParagraphs {
 
-	static String dir = "/projects/pardosa/s5/raphaelh/tac";
-
-	//static String in = dir + "/data/source_09nw";
-	static String in = dir + "/data/source_10wb";
-	static String out1 = dir + "/data/10wb/paragraphs.text";
-	static String out2 = dir + "/data/10wb/paragraphs.meta";
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedWriter w1 = new BufferedWriter(new OutputStreamWriter
-				(new FileOutputStream(out1), "utf-8"));
-		BufferedWriter w2 = new BufferedWriter(new OutputStreamWriter
-				(new FileOutputStream(out2), "utf-8"));
-		BufferedReader r = new BufferedReader(new InputStreamReader
-				(new FileInputStream(in), "utf-8"));
-		String l = null;
-		int i = 0;
-		while ((l = r.readLine())!= null) {
-			String[] c = l.split("\t");
-			if (c.length < 5) {System.out.println(l); continue; }
-			w1.write(i + "\t" + c[2] + "\n");
-			w2.write(i + "\t" + c[0] + "\t" + /*c[1] + "\t" +*/ c[3] + "\t" + c[4] + "\n");
-			i++;
-		}
-		r.close();
-		w1.close();
-		w2.close();
+	private PrintStream metaOutput;
+	private PrintStream textOutput;
+	private BufferedReader sentencesInput;
+	
+	public CreateParagraphs(BufferedReader sentencesInput, PrintStream metaOutput, PrintStream textOutput) {
+		
+		this.sentencesInput = sentencesInput;
+		this.textOutput = textOutput;
+		this.metaOutput = metaOutput;
+		
 	}
 
+	public void createParagraphs() throws IOException {
+
+		String l = null;
+		int i = 0;
+		while ((l = sentencesInput.readLine())!= null) {
+			String[] c = l.split("\t");
+			if (c.length < 5) {System.out.println(l); continue; }
+			metaOutput.print(i + "\t" + c[2] + "\n");
+			textOutput.print(i + "\t" + c[0] + "\t" + /*c[1] + "\t" +*/ c[3] + "\t" + c[4] + "\n");
+			i++;
+		}
+	}
+
+	public void close() throws IOException {
+		sentencesInput.close();
+		textOutput.close();
+		metaOutput.close();
+	}
+	
+	public static void main(String[] args) throws IOException {
+	
+		String inputFile = args[0];
+		String textOutputFile = args[1];
+		String metaOutputFile = args[2];
+		
+		BufferedReader rawInput;
+		if (inputFile.equals("stdin")) rawInput = new BufferedReader(new InputStreamReader(System.in));
+		else rawInput = new BufferedReader(new FileReader(inputFile));
+		
+		PrintStream textOutput;
+		if (textOutputFile.equals("stdout")) textOutput = System.out;
+		else textOutput = new PrintStream(textOutputFile);
+		
+		PrintStream metaOutput = new PrintStream(metaOutputFile);
+		
+		CreateParagraphs cp = new CreateParagraphs(rawInput, textOutput, metaOutput);
+		
+		cp.createParagraphs();
+		
+		cp.close();
+	}
 }
