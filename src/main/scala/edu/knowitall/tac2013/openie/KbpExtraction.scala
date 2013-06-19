d933eada4af7c11d757f3ba509f15f07e430f4a8
Rob Bart <rbart@cs.washington.edu>
2013-06-19 13:10:02 -0700

    Added to SolrPopulator skeleton.

--- src/main/scala/edu/knowitall/tac2013/openie/KbpExtraction.scala
+++ src/main/scala/edu/knowitall/tac2013/openie/KbpExtraction.scala
@@ -1,14 +1,36 @@
 package edu.knowitall.tac2013.openie
 
-class KbpExtraction(
+case class KbpExtraction(
   val arg1: String,
   val rel: String,
   val arg2: String,
+  val arg1postags: String,
+  val relpostags: String,
+  val arg2postags: String,
   val docId: String,
   val sentId: String,
   val sent: String,
   val sentStartOffset: String,
   val sentEndOffset: String
-) {
+)
 
+object KbpExtraction {
+  
+  def toString(e: KbpExtraction): String = {
+    val fields = Seq(e.arg1, e.rel, e.arg2, e.arg1postags, e.relpostags, e.arg2postags, e.docId, e.sentId, e.sent, e.sentStartOffset, e.sentEndOffset)
+    fields.map(field => field.replaceAll("\t", " ")).mkString("\t")
+  }
+ 
+  def fromString(str: String): Option[KbpExtraction] = {
+    
+    str.split("\t") match {
+      case Array(arg1, rel, arg2, arg1postags, relpostags, arg2postags, docId, sentId, sent, sentStartOffset, sentEndOffset, _*) => {
+        Some(new KbpExtraction(arg1, rel, arg2, arg1postags, relpostags, arg2postags, docId, sentId, sent, sentStartOffset, sentEndOffset))
+      }
+      case _ => {
+        System.err.println("Error parsing KbpExtraction: %s".format(str))
+        None
+      }
+    }
+  }
 }
--- src/main/scala/edu/knowitall/tac2013/solr/KbpSolrPopulator.scala
+++ src/main/scala/edu/knowitall/tac2013/solr/KbpSolrPopulator.scala
@@ -1,5 +1,55 @@
 package edu.knowitall.tac2013.solr
 
-class KbpSolrPopulator {
+import edu.knowitall.tac2013.openie.KbpExtraction
+import org.apache.solr.common.SolrInputDocument
+import org.apache.solr.client.solrj.SolrServer
 
+class KbpSolrPopulator(solrServer: SolrServer) extends SolrPopulator[KbpExtraction](solrServer) {
+
+  def toSolrInputDocument(e: KbpExtraction): SolrInputDocument = {
+    val doc = new SolrInputDocument()
+    doc.addField("arg1", e.arg1)
+    doc.addField("rel", e.rel)
+    doc.addField("arg2", e.arg2)
+    doc.addField("arg1_postag", e.arg1postags)
+    doc.addField("rel_postag", e.relpostags)
+    doc.addField("arg2_postag", e.arg2postags)
+    doc
+  }
 }
+
+/*
+<doc>
+            <field name="id">{ extr.id }</field>
+
+            <field name="arg1">{ extr.arg1 }</field>
+            <field name="rel">{ extr.rel }</field>
+            { extr.arg2s.map { arg2 =>
+              <field name="arg2">{ arg2 }</field>
+            }}
+
+            <field name="arg1_postag">{ extr.sentence.tokens(extr.arg1Interval).map(_.postag).mkString(" ") }</field>
+            <field name="rel_postag">{ extr.sentence.tokens(extr.relInterval).map(_.postag).mkString(" ") }</field>
+            <field name="arg2_postag">{ extr.sentence.tokens(extr.arg2Interval).map(_.postag).mkString(" ") }</field>
+
+            { extr.arg1Types(sentenceEntity.types).map { typ =>
+              <field name="arg1_types">{ typ.descriptor }</field>
+            }}
+            { extr.relTypes(sentenceEntity.types).map { typ =>
+              <field name="rel_types">{ typ.descriptor }</field>
+            }}
+            { extr.arg2Types(sentenceEntity.types).map { typ =>
+              <field name="arg2_types">{ typ.descriptor }</field>
+            }}
+
+            <field name="context"></field>
+
+            <field name="confidence">{ extr.confidence }</field>
+
+            <field name="sentence">{ sentenceEntity.text }</field>
+
+            <field name="extractor">{ extr.extractor }</field>
+            <field name="url">{ sentenceEntity.document.path }</field>
+            <field name="corpus">{ sentenceEntity.document.corpus }</field>
+          </doc>
+*/
