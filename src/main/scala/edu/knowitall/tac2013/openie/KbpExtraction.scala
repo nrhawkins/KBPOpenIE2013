820f81a1dbf726197257d33e0fc80adb6e8fce3b
Rob Bart <rbart@cs.washington.edu>
2013-06-21 10:49:37 -0700

    Get postags directly from ExtractionPart.

--- src/main/scala/edu/knowitall/tac2013/openie/KbpExtraction.scala
+++ src/main/scala/edu/knowitall/tac2013/openie/KbpExtraction.scala
@@ -7,6 +7,7 @@ import edu.knowitall.chunkedextractor.BinaryExtractionInstance
 import edu.knowitall.chunkedextractor.Relnoun
 import edu.knowitall.srlie.confidence.SrlConfidenceFunction.SrlConfidenceFunction
 import edu.knowitall.tool.chunk.ChunkedToken
+import edu.knowitall.chunkedextractor.ExtractionPart
 
 case class KbpExtraction(
   val arg1: String,
@@ -47,19 +48,17 @@ object KbpExtraction {
       relnounInst: BinaryExtractionInstance[Relnoun.Token], 
       parsedSentence: ParsedKbpSentence): KbpExtraction = {
     
-    val tokens = relnounInst.sent
-    
     val extr = relnounInst.extr
     
-    def postags(interval: Interval) = tokens.drop(interval.start).take(interval.size).map(_.postag).mkString(" ")
+    def postags(epart: ExtractionPart[ChunkedToken]) = epart.tokens.map(_.postag).mkString(" ")
     
     new KbpExtraction(
         arg1 = extr.arg1.text,
         rel = extr.rel.text,
         arg2 = extr.arg2.text,
-        arg1postags = postags(extr.arg1.interval),
-        relpostags = postags(extr.rel.interval), 
-        arg2postags = postags(extr.arg2.interval),
+        arg1postags = postags(extr.arg1),
+        relpostags = postags(extr.rel), 
+        arg2postags = postags(extr.arg2),
         confidence = "0.9",
         extractor = "relnoun")
   }
