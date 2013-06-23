359590a36167f2cca4067f44580188d41766198f
Rob Bart <rbart@cs.washington.edu>
2013-06-22 23:37:44 -0700

    Move tests to correct source files.

--- src/test/scala/edu/knowitall/tac2013/prep/KbpSentenceSpec.scala
+++ src/test/scala/edu/knowitall/tac2013/prep/KbpSentenceSpec.scala
@@ -2,15 +2,18 @@ package edu.knowitall.tac2013.prep
 
 import org.scalatest._
 
-class ParsedKbpSentenceSpec extends FlatSpec {
+class KbpSentenceSpec extends FlatSpec {
 
-  "ParsedKbpSentences" should "deserialize then serialize to their original string" in {
+  val samplesDir = "src/main/resources/samples/"
+  val files = Seq("news", "web", "forum").map { c => "%s%s%s".format(samplesDir, c, "-sentences.txt") }
+  
+  "KbpSentences" should "deserialize then serialize to their original string" in {
     
-    val testSrc = scala.io.Source.fromFile("src/main/resources/samples/parsedKbpSentences.1k")
-    val lines = testSrc.getLines
+    val testSrcs = files map scala.io.Source.fromFile
+    val lines = testSrcs.flatMap(_.getLines)
     lines.foreach { line =>
-      val sent = ParsedKbpSentence.read(line).get
-      val reserialized = ParsedKbpSentence.write(sent)
+      val sent = KbpSentence.read(line).get
+      val reserialized = KbpSentence.write(sent)
       require(reserialized.equals(line))
     } 
   }
--- src/test/scala/edu/knowitall/tac2013/prep/ParsedKbpSentenceSpec.scala
+++ src/test/scala/edu/knowitall/tac2013/prep/ParsedKbpSentenceSpec.scala
@@ -1,17 +1,16 @@
 package edu.knowitall.tac2013.prep
 
 import org.scalatest._
-import edu.knowitall.tac2013.prep.KbpSentence
 
-class KbpSentenceSpec extends FlatSpec {
+class ParsedKbpSentenceSpec extends FlatSpec {
 
-  "KbpSentences" should "deserialize then serialize to their original string" in {
+  "ParsedKbpSentences" should "deserialize then serialize to their original string" in {
     
-    val testSrc = scala.io.Source.fromFile("src/main/resources/samples/sentences.prep.1k")
+    val testSrc = scala.io.Source.fromFile("src/main/resources/samples/parsedKbpSentences.1k")
     val lines = testSrc.getLines
     lines.foreach { line =>
-      val sent = KbpSentence.read(line).get
-      val reserialized = KbpSentence.write(sent)
+      val sent = ParsedKbpSentence.read(line).get
+      val reserialized = ParsedKbpSentence.write(sent)
       require(reserialized.equals(line))
     } 
   }
