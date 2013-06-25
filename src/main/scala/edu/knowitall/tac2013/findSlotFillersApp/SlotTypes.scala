74aa3a484a6f1f0843ae9a3d1f9cab987e6ed682
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-06-25 14:00:03 -0700

    Wrote that code that parses a single xml file with a single KBP query and builds a KBPQuery class instance with this information for later processing.

--- src/main/scala/edu/knowitall/tac2013/findSlotFillersApp/SlotTypes.scala
+++ src/main/scala/edu/knowitall/tac2013/findSlotFillersApp/SlotTypes.scala
@@ -3,18 +3,18 @@ package edu.knowitall.tac2013.findSlotFillersApp
 import scala.io.Source
 
 object SlotTypes {
-  var personSlotTypes = List[String]()
-  var organizationSlotTypes = List[String]()
+  var personSlotTypes = Set[String]()
+  var organizationSlotTypes = Set[String]()
   private val personSource = Source.fromURL(getClass.getResource("/edu/knowitall/tac2013/findSlotFillersApp/PersonSlotTypes.txt"))
   private val organizationSource = Source.fromURL(getClass.getResource("/edu/knowitall/tac2013/findSlotFillersApp/OrganizationSlotTypes.txt"))
   personSource.getLines.foreach(l => {
 	  if (l.trim().contains("per:")){
-	    personSlotTypes = l.trim() :: personSlotTypes
+	    personSlotTypes = personSlotTypes + l.trim()
 	  }
   })
   organizationSource.getLines.foreach(l => {
 	  if (l.trim().contains("org:")){
-	    organizationSlotTypes = l.trim() :: organizationSlotTypes
+	    organizationSlotTypes = organizationSlotTypes + l.trim()
 	  }
   })
 }
