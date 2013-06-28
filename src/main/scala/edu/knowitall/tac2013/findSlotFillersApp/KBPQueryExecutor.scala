2e7ab6e5ed7e890e9c8b1fe7c8f2d0908127e4f2
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-06-27 17:42:11 -0700

    Edited KBPQueryExecutor

--- src/main/scala/edu/knowitall/tac2013/findSlotFillersApp/KBPQueryExecutor.scala
+++ src/main/scala/edu/knowitall/tac2013/findSlotFillersApp/KBPQueryExecutor.scala
@@ -1,8 +1,41 @@
 package edu.knowitall.tac2013.findSlotFillersApp
 
+import QueryEntityForAllSlots.executeEntityQueryForAllSlots
+import KBPSlotOpenIERelationTranslator.getOrganizationMap
+import KBPSlotOpenIERelationTranslator.getPersonMap
+import KbpQueryOutput.printFormattedOutput
+
 object KBPQueryExecutor {
   
 
+  def executeKbpQuery(kbpQuery: KBPQuery, outputPath: String){
+    kbpQuery.entityType match {
+      case KBPQueryEntityType.ORG => {
+        val orgMap = getOrganizationMap()
+        
+        //filter out ignored slots
+        var filteredOrgMap = Map[String,List[KbpSlotToOpenIEData]]()
+        for(slot <- kbpQuery.slotsToFill){
+          filteredOrgMap += (slot -> orgMap(slot))
+        }
+        val results = executeEntityQueryForAllSlots(kbpQuery.name,filteredOrgMap)
+         printFormattedOutput(results,outputPath,KBPQueryEntityType.ORG)       
+      }
+      case KBPQueryEntityType.PER => {
+        val perMap = getPersonMap()
+        
+        //filter out ignored slots
+        var filteredPerMap = Map[String,List[KbpSlotToOpenIEData]]()
+        for(slot <- kbpQuery.slotsToFill){
+          filteredPerMap += (slot -> perMap(slot))
+        }
+        val results = executeEntityQueryForAllSlots(kbpQuery.name,filteredPerMap)
+        printFormattedOutput(results,outputPath,KBPQueryEntityType.PER)
+      }
+      case _ => throw new Exception("Entity Type must be person or organization")
+    }
+  }
+  
     
   def main(args: Array[String]){
 
@@ -10,11 +43,10 @@ object KBPQueryExecutor {
          "there should be two arguments: path to KBP query Files, path for outputFile")
          
      val KBPQueryPath = args(0)
-     val OutputPath = args(1)
+     val outputPath = args(1)
      
-     val KbpQuery = KBPQuery.parseKBPQuery(KBPQueryPath)
-     println(KbpQuery.slotsToFill)
-         
+     val kbpQuery = KBPQuery.parseKBPQuery(KBPQueryPath)
+     executeKbpQuery(kbpQuery,outputPath)    
   }
          
      
