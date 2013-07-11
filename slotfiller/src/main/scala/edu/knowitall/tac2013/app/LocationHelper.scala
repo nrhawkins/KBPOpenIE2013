302f9268c7c1a92416f47f696f17d80d17942cfe
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-07-11 16:57:06 -0700

    Changed location filtering code to trust NELL if there is only one tag in the slot field

--- slotfiller/src/main/scala/edu/knowitall/tac2013/app/LocationHelper.scala
+++ slotfiller/src/main/scala/edu/knowitall/tac2013/app/LocationHelper.scala
@@ -15,22 +15,74 @@ object LocationHelper {
   def findLocationTaggedType(typesInSlotFill : List[Type], slotType: String): Option[Type] = {
     val locationTypesInSlotFill = typesInSlotFill.filter(t => (t.descriptor() == "StanfordLOCATION"))
     if(locationTypesInSlotFill.nonEmpty){
+      
+      
 	    if(slotType == "City"){
-	      //only look at leftmost tag
 	      val firstTag = locationTypesInSlotFill.head
-	      if (isCity(firstTag.text())){
-	        return Some(firstTag)
+	      //if there is only one tag, then if the name is in NELL it should only 
+	      //be accepted as a city if city has the highest probability
+	      if(locationTypesInSlotFill.length  == 1){
+	        val nellData = NellData.getNellData(firstTag.text())
+	        if(nellData.isDefined){
+		        if(nellData.get.highestProbabilityIsCity && isCity(firstTag.text())){
+		          return Some(firstTag)
+		        }
+		        else{
+		          return None
+		        }
+		      }
+	         else{
+	            if (isCity(firstTag.text())){
+		          return Some(firstTag)
+		        }
+		        return None
+	         }
 	      }
-	      return None
+	      //else if more than one tag only look at the left most tag and accept it
+	      //as a city if it appears anywhere as a city
+	      else{
+	           //only look at leftmost tag
+		      if (isCity(firstTag.text())){
+		        return Some(firstTag)
+		      }
+		      return None
+	      }
 	    }
+	    
+	    
 	    else if(slotType=="Country"){
 	      //only look at right most tag
 	      val lastTag = locationTypesInSlotFill(locationTypesInSlotFill.length-1)
-	      if(isCountry(lastTag.text())){
-	        return Some(lastTag)
+	      
+	      if(locationTypesInSlotFill.length ==1){
+	        val nellData = NellData.getNellData(lastTag.text())
+	        if(nellData.isDefined){
+	          if(nellData.get.highestProbabilityIsCountry && isCountry(lastTag.text())){
+	            return Some(lastTag)
+	          }
+	          else{
+	            return None
+	          }
+	        }
+	        else{
+		      if(isCountry(lastTag.text())){
+		        return Some(lastTag)
+		      }
+		      return None
+	        }
 	      }
-	      return None
+	      else{
+		      if(isCountry(lastTag.text())){
+		        return Some(lastTag)
+		      }
+		      return None
+	      }
 	    }
+	    
+	    
+	    
+	    
+	    
 	    else if(slotType=="Stateorprovince"){
 	      //if more than 2 tags look at 2nd tag only otherwise look at the only tag
 	      var tag = locationTypesInSlotFill.head
@@ -38,11 +90,34 @@ object LocationHelper {
 	        tag = locationTypesInSlotFill(1)
 	      }
 	      
-	      if(isStateOrProvince(tag.text())){
-	        return Some(tag)
+	      
+	      if(locationTypesInSlotFill.length ==1){
+	        val nellData = NellData.getNellData(tag.text())
+	        if(nellData.isDefined){
+	          if(nellData.get.highestProbabilityIsStateOrProvince && isStateOrProvince(tag.text())){
+	            return Some(tag)
+	          }
+	          else{
+	            return None
+	          }
+	        }
+	        else{
+		      if(isStateOrProvince(tag.text())){
+		        return Some(tag)
+		      }
+		      else{
+		    	  return None
+		      }	          
+	        }
 	      }
-
-	      return None
+	      else{
+		      if(isStateOrProvince(tag.text())){
+		        return Some(tag)
+		      }
+		      else{
+		    	  return None
+		      }
+	      }
 	    }
 	    else{
 	      throw new Exception("There should only be three types of locations specified in the pattern, Country, Stateorprovince, and City")
@@ -60,11 +135,17 @@ object LocationHelper {
   }
   
   def isStateOrProvince(str: String): Boolean = {
-    if (TipsterData.stateOrProvinces.contains(str.toLowerCase()) || (NellData.stateOrProvinceNameSet.contains(str.toLowerCase()))) true else false
+    if ( (TipsterData.stateOrProvinces.contains(str.toLowerCase()) ||
+        (NellData.stateOrProvinceNameSet.contains(str.toLowerCase()))
+         && 
+         (!str.toLowerCase().contains("city")))) true else false
   }
   
   def isCountry(str: String): Boolean = {
-    if (TipsterData.countries.contains(str.toLowerCase()) || (NellData.countryNameSet.contains(str.toLowerCase()))) true else false
+    if ( (TipsterData.countries.contains(str.toLowerCase()) || 
+        (NellData.countryNameSet.contains(str.toLowerCase()))     
+         &&
+         (!str.toLowerCase().contains("city")))) true else false
   }
 
 }
